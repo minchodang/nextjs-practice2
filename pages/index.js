4636b2214d96f83e733924c02c0b663e344adc3c
minsukang <minsu910725@gmail.com>
2022-04-03 08:36:43 +0900

    Add: static preRendering

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Home() {
+  const [counter, setCounter] = useState(0);
   return (
     <>
-      <h1>hello </h1>
-      <button>
-        <Link href="/about">about</Link>
-      </button>
+      <h1>Hello {counter}</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>countUp</button>
     </>
   );
 }
